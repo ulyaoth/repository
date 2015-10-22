7ad04492c4ad43c4035de65f2f850fa51d1bc96a
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2015-10-22 20:04:00 +0200

    adding SRPM.

--- ulyaoth-monkey/build-ulyaoth-monkey.sh
+++ ulyaoth-monkey/build-ulyaoth-monkey.sh
@@ -42,6 +42,7 @@ fi
 
 su ulyaoth -c "spectool ulyaoth-monkey.spec -g -R"
 su ulyaoth -c "rpmbuild -ba ulyaoth-monkey.spec"
+cp /home/ulyaoth/rpmbuild/SRPMS/* /root/
 cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
 cp /home/ulyaoth/rpmbuild/RPMS/i686/* /root/
 cp /home/ulyaoth/rpmbuild/RPMS/i386/* /root/
