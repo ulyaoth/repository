912d425c87b5967e27c49c5f500f522a9a8e2e3a
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2016-01-01 16:11:09 +0100

    test fix.

--- ulyaoth-logstash/build-ulyaoth-logstash.sh
+++ ulyaoth-logstash/build-ulyaoth-logstash.sh
@@ -16,7 +16,17 @@ su ulyaoth -c "rpmdev-setuptree"
 
 su ulyaoth -c "wget https://github.com/elastic/logstash/archive/v$version.tar.gz"
 su ulyaoth -c "tar xvzf v$version.tar.gz"
+
+if grep -q -i "release 6" /etc/redhat-release
+then
+yum install libyaml-devel readline-devel zlib-devel libffi-devel openssl-devel sqlite-devel -y
+su - ulyaoth -c "gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
+su - ulyaoth -c "\curl -sSL https://get.rvm.io | bash -s stable --gems=rake"
+su - ulyaoth -c "cd /home/ulyaoth/logstash-2.1.1 && rake artifact:tar"
+else
 su ulyaoth -c "cd logstash-$version && rake artifact:tar"
+fi
+
 su ulyaoth -c "cp /home/ulyaoth/logstash-$version/build/logstash-$version.tar.gz /home/ulyaoth/rpmbuild/SOURCES/"
 rm -rf /home/ulyaoth/logstash-$version
 rm -rf /home/ulyaoth/v$version.tar.gz
