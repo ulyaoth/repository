11663d758d2a25a27936329d567835688bc3550a
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2016-04-03 19:59:49 +0200

    Kafka 8 start
    #47

---
+++ ulyaoth-kafka/build-ulyaoth-kafka8-scala2.11.sh
@@ -0,0 +1,21 @@
+buildarch="$(uname -m)"
+
+useradd ulyaoth
+cd /home/ulyaoth
+su ulyaoth -c "rpmdev-setuptree"
+cd /home/ulyaoth/rpmbuild/SPECS/
+su ulyaoth -c "wget https://raw.githubusercontent.com/ulyaoth/repository/master/ulyaoth-kafka/SPECS/ulyaoth-kafka8-scala2.11.spec"
+
+if [ "$arch" != "x86_64" ]
+then
+sed -i '/BuildArch: x86_64/c\BuildArch: '"$buildarch"'' ulyaoth-kafka8-scala2.11.spec
+fi
+
+su ulyaoth -c "spectool ulyaoth-kafka8-scala2.11.spec -g -R"
+su ulyaoth -c "rpmbuild -ba ulyaoth-kafka8-scala2.11.spec"
+cp /home/ulyaoth/rpmbuild/SRPMS/* /root/
+cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
+cp /home/ulyaoth/rpmbuild/RPMS/i686/* /root/
+cp /home/ulyaoth/rpmbuild/RPMS/i386/* /root/
+su ulyaoth -c "rm -rf /home/ulyaoth/rpmbuild"
+rm -rf /root/build-ulyaoth-kafka8-scala2.11.sh
