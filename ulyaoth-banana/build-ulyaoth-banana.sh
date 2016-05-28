a9ccab3daa3adf76a53da154c2b726ec16aba643
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2016-05-28 10:03:32 +0200

    Fix build

--- ulyaoth-banana/build-ulyaoth-banana.sh
+++ ulyaoth-banana/build-ulyaoth-banana.sh
@@ -31,7 +31,7 @@ su ulyaoth -c "tar xvzf v'"$bananaversion"'.tar.gz"
 su ulyaoth -c "mkdir -p /home/ulyaoth/banana-'"$bananaversion"'/build"
 cd /home/ulyaoth/banana-$bananaversion
 su ulyaoth -c "ant"
-mv /home/ulyaoth/banana-$bananaversion/build/banana-1.6.0.war /home/ulyaoth/rpmbuild/SOURCES/banana.war
+mv /home/ulyaoth/banana-$bananaversion/build/banana-$bananaversion.war /home/ulyaoth/rpmbuild/SOURCES/banana.war
 cd /home/ulyaoth/rpmbuild/SPECS/
 
 su ulyaoth -c "rpmbuild -ba ulyaoth-banana.spec"
