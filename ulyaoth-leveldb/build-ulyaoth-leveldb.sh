12a62f61813dea9a71f7adb9577fceb2b0027eea
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2016-06-26 16:40:45 +0200

    Leveldb 1.18 initial release.
    #707

---
+++ ulyaoth-leveldb/build-ulyaoth-leveldb.sh
@@ -0,0 +1,75 @@
+ulyaothos=`cat /etc/ulyaoth`
+buildarch="$(uname -m)"
+
+useradd ulyaoth
+usermod -Gulyaoth ulyaoth
+cd /home/ulyaoth/
+su ulyaoth -c "rpmdev-setuptree"
+cd /home/ulyaoth/rpmbuild/SPECS
+su ulyaoth -c "wget https://raw.githubusercontent.com/ulyaoth/repository/master/ulyaoth-fuse-s3fs/SPECS/ulyaoth-fuse-s3fs.spec"
+
+if [ "$arch" != "x86_64" ]
+then
+sed -i '/BuildArch: x86_64/c\BuildArch: '"$buildarch"'' ulyaoth-fuse-s3fs.spec
+fi
+
+if [ "$ulyaothos" == "fedora" ]
+then
+if type dnf 2>/dev/null
+then
+  dnf install https://downloads.ulyaoth.net/rpm/ulyaoth-latest.fedora.noarch.rpm -y
+elif type yum 2>/dev/null
+then
+  yum install https://downloads.ulyaoth.net/rpm/ulyaoth-latest.fedora.noarch.rpm -y
+fi
+elif [ "$ulyaothos" == "redhat" ]
+then
+  yum install https://downloads.ulyaoth.net/rpm/ulyaoth-latest.redhat.noarch.rpm -y
+elif [ "$ulyaothos" == "amazonlinux" ]
+then
+  yum install https://downloads.ulyaoth.net/rpm/ulyaoth-latest.amazonlinux.noarch.rpm -y
+elif [ "$ulyaothos" == "centos" ]
+then
+  yum install https://downloads.ulyaoth.net/rpm/ulyaoth-latest.centos.noarch.rpm -y
+elif [ "$ulyaothos" == "oraclelinux" ]
+then 
+  yum install https://downloads.ulyaoth.net/rpm/ulyaoth-latest.oraclelinux.noarch.rpm -y
+elif [ "$ulyaothos" == "scientificlinux" ]
+then
+  yum install https://downloads.ulyaoth.net/rpm/ulyaoth-latest.scientificlinux.noarch.rpm -y
+fi
+
+if type dnf 2>/dev/null
+then
+  dnf remove fuse fuse-libs -y
+elif type yum 2>/dev/null
+then
+  yum remove fuse fuse-libs -y
+fi
+
+if type dnf 2>/dev/null
+then
+  dnf builddep -y ulyaoth-fuse-s3fs.spec
+elif type yum 2>/dev/null
+then
+  yum-builddep -y ulyaoth-fuse-s3fs.spec
+fi
+
+su ulyaoth -c "spectool ulyaoth-fuse-s3fs.spec -g -R"
+su ulyaoth -c "rpmbuild -ba ulyaoth-fuse-s3fs.spec"
+
+if [ "$ulyaothos" == "amazonlinux" ]
+then
+  cp /home/ulyaoth/rpmbuild/SRPMS/* /home/ec2-user/
+  cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /home/ec2-user/
+  cp /home/ulyaoth/rpmbuild/RPMS/i686/* /home/ec2-user/
+  cp /home/ulyaoth/rpmbuild/RPMS/i386/* /home/ec2-user/
+else
+  cp /home/ulyaoth/rpmbuild/SRPMS/* /root/
+  cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
+  cp /home/ulyaoth/rpmbuild/RPMS/i686/* /root/
+  cp /home/ulyaoth/rpmbuild/RPMS/i386/* /root/
+fi
+
+rm -rf /root/build-ulyaoth-*
+rm -rf /home/ulyaoth/rpmbuild
