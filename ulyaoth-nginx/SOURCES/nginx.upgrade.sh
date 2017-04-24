4ad5c07abc6434789435ff00eb728dfa4cad6c75
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2017-04-24 20:28:40 +0200

    Multiple fixes

--- ulyaoth-nginx/SOURCES/nginx.upgrade.sh
+++ ulyaoth-nginx/SOURCES/nginx.upgrade.sh
@@ -5,12 +5,16 @@
 # Source function library.
 [ -f /etc/rc.d/init.d/functions ] && . /etc/rc.d/init.d/functions
 
+if [ -f /etc/sysconfig/nginx ]; then
+    . /etc/sysconfig/nginx
+fi
+
 prog=nginx
 nginx=/usr/sbin/nginx
 conffile=/etc/nginx/nginx.conf
 pidfile=`/usr/bin/systemctl show -p PIDFile nginx.service | sed 's/^PIDFile=//' | tr ' ' '\n'`
-SLEEPMSEC=200000
-UPGRADEWAITLOOPS=5
+SLEEPSEC=${SLEEPSEC:-1}
+UPGRADEWAITLOOPS=${UPGRADEWAITLOOPS:-5}
 
 oldbinpidfile=${pidfile}.oldbin
 ${nginx} -t -c ${conffile} -q || return 6
@@ -19,7 +23,7 @@ killproc -p ${pidfile} ${prog} -USR2
 echo
 
 for i in `/usr/bin/seq $UPGRADEWAITLOOPS`; do
-    /bin/usleep $SLEEPMSEC
+    /bin/sleep $SLEEPSEC
     if [ -f ${oldbinpidfile} -a -f ${pidfile} ]; then
         echo -n $"Graceful shutdown of old $prog: "
         killproc -p ${oldbinpidfile} ${prog} -QUIT
