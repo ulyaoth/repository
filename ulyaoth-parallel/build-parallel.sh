9116a3c8dcfec501b02edfbbbb29e8971a67be2a
Sjir Bagmeijer <sbagmeijer@ulyaoth.net>
2017-11-04 18:32:59 +0100

    adding EC2 build script

---
+++ ulyaoth-parallel/build-parallel.sh
@@ -0,0 +1,15 @@
+# This script is supposed to run as the user "ulyaoth".
+
+ulyaothos=`cat /etc/ulyaoth`
+
+rpmdev-setuptree
+cd /home/ulyaoth/rpmbuild/SPECS/
+
+wget https://raw.githubusercontent.com/ulyaoth/repository/master/ulyaoth-parallel/SPECS/ulyaoth-parallel.spec -O /home/ulyaoth/rpmbuild/SPECS/ulyaoth-parallel.spec
+
+spectool /home/ulyaoth/rpmbuild/SPECS/ulyaoth-parallel.spec -g -R
+rpmbuild -ba /home/ulyaoth/rpmbuild/SPECS/ulyaoth-parallel.spec
+
+cp /home/ulyaoth/rpmbuild/SRPMS/* /home/ulyaoth/
+cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /home/ulyaoth/
+cp /home/ulyaoth/rpmbuild/RPMS/noarch/* /home/ulyaoth/
