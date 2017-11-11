569f412b6d0f273fc705c55f3ac012244fc44e46
Sjir Bagmeijer <sbagmeijer@ulyaoth.net>
2017-11-11 17:43:59 +0100

    fix build

--- ulyaoth-solr/build-solr5.sh
+++ ulyaoth-solr/build-solr5.sh
@@ -4,6 +4,11 @@ version=5.5.5
 # create build environment.
 rpmdev-setuptree
 
+# Download the spec files
+wget https://raw.githubusercontent.com/ulyaoth/repository/master/ulyaoth-solr/SPECS/ulyaoth-solr5.spec -O /home/ulyaoth/rpmbuild/SPECS/ulyaoth-solr5.spec
+wget https://raw.githubusercontent.com/ulyaoth/repository/master/ulyaoth-solr/SPECS/ulyaoth-solr5-examples.spec -O /home/ulyaoth/rpmbuild/SPECS/ulyaoth-solr5-examples.spec
+wget https://raw.githubusercontent.com/ulyaoth/repository/master/ulyaoth-solr/SPECS/ulyaoth-solr5-docs.spec -O /home/ulyaoth/rpmbuild/SPECS/ulyaoth-solr5-docs.spec
+
 # Downloads solr 5 package and prepare for rpm build.
 wget http://apache.mirrors.spacedump.net/lucene/solr/$version/solr-$version.tgz
 tar xvf solr-$version.tgz
@@ -15,9 +20,6 @@ rm -rf /home/ulyaoth/solr-$version/bin/solr.in.sh
 rm -rf /home/ulyaoth/solr-$version/server/resources/log4j.properties
 tar cvf solr-$version.tar.gz solr-$version/
 mv solr-$version.tar.gz /home/ulyaoth/rpmbuild/SOURCES/
-
-# Download spec file.
-wget https://raw.githubusercontent.com/ulyaoth/repository/master/ulyaoth-solr/SPECS/ulyaoth-solr5.spec -O /home/ulyaoth/rpmbuild/SPECS/ulyaoth-solr5.spec
 
 # Install the solr requirements for building the rpm.
 if type dnf 2>/dev/null
@@ -34,10 +36,9 @@ spectool /home/ulyaoth/rpmbuild/SPECS/ulyaoth-solr5.spec -g -R
 # Build Solr 5 rpm.
 rpmbuild -ba /home/ulyaoth/rpmbuild/SPECS/ulyaoth-solr5.spec
 
-cp /home/ulyaoth/rpmbuild/SRPMS/* /home/ulyaoth/
-cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /home/ulyaoth/
-cp /home/ulyaoth/rpmbuild/RPMS/noarch/* /home/ulyaoth/
-rm -rf /home/ulyaoth/rpmbuild
+# Clean
+rm -rf /home/ulyaoth/rpmbuild/BUILD
+rm -rf /home/ulyaoth/rpmbuild/SOURCES
 rm -rf /home/ulyaoth/solr
 rm -rf /home/ulyaoth/solr-$version
 
@@ -51,12 +52,11 @@ tar cvf solr-$version.tar.gz solr-$version/
 mv solr-$version.tar.gz /home/ulyaoth/rpmbuild/SOURCES/
 
 # Build solr 5 examples rpm.
-wget https://raw.githubusercontent.com/ulyaoth/repository/master/ulyaoth-solr/SPECS/ulyaoth-solr5-examples.spec -O /home/ulyaoth/rpmbuild/SPECS/ulyaoth-solr5-examples.spec
-rpmbuild -ba ulyaoth-solr5-examples.spec
-cp /home/ulyaoth/rpmbuild/SRPMS/* /home/ulyaoth/
-cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /home/ulyaoth/
-cp /home/ulyaoth/rpmbuild/RPMS/noarch/* /home/ulyaoth/
-rm -rf /home/ulyaoth/rpmbuild
+rpmbuild -ba /home/ulyaoth/rpmbuild/SPECS/ulyaoth-solr5-examples.spec
+
+# Clean
+rm -rf /home/ulyaoth/rpmbuild/BUILD
+rm -rf /home/ulyaoth/rpmbuild/SOURCES
 rm -rf /home/ulyaoth/solr
 rm -rf /home/ulyaoth/solr-$version
 
@@ -70,11 +70,9 @@ tar cvf solr-$version.tar.gz solr-$version/
 mv solr-$version.tar.gz /home/ulyaoth/rpmbuild/SOURCES/
 
 # Build Solr 5 Documentation rpm.
-wget https://raw.githubusercontent.com/ulyaoth/repository/master/ulyaoth-solr/SPECS/ulyaoth-solr5-docs.spec -O /home/ulyaoth/rpmbuild/SPECS/ulyaoth-solr5-docs.spec
-rpmbuild -ba ulyaoth-solr5-docs.spec
+rpmbuild -ba /home/ulyaoth/rpmbuild/SPECS/ulyaoth-solr5-docs.spec
+
+# Copy the file to Ulyaoth home folder.
 cp /home/ulyaoth/rpmbuild/SRPMS/* /home/ulyaoth/
 cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /home/ulyaoth/
 cp /home/ulyaoth/rpmbuild/RPMS/noarch/* /home/ulyaoth/
-rm -rf /home/ulyaoth/rpmbuild
-rm -rf /home/ulyaoth/solr
-rm -rf /home/ulyaoth/solr-$version
