c8814a9c94f9347c1a10ea2797be12ab037533ba
Sjir Bagmeijer <sbagmeijer@ulyaoth.net>
2017-11-19 19:06:52 +0100

    updated

--- ulyaoth-tengine/build-tengine.sh
+++ ulyaoth-tengine/build-tengine.sh
@@ -1,53 +1,31 @@
-ulyaothos=`cat /etc/ulyaoth`
-arch="$(uname -m)"
-buildarch="$(uname -m)"
+# This script is supposed to run as the user "ulyaoth".
 
-
-if grep -q -i "release 6" /etc/redhat-release
-then
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-elif grep -q -i "release 6" /etc/centos-release
-then
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-elif grep -q -i "release 7" /etc/oracle-release
-then
-yum install -y http://mirror.centos.org/centos/7/os/x86_64/Packages/GeoIP-devel-1.5.0-9.el7.x86_64.rpm
-else
-echo No extra installation required for this OS!
-fi
-
-useradd ulyaoth
-usermod -Gulyaoth ulyaoth
-cd /home/ulyaoth/
-su ulyaoth -c "rpmdev-setuptree"
-cd /home/ulyaoth/rpmbuild/SPECS
-su ulyaoth -c "wget https://raw.githubusercontent.com/ulyaoth/repository/master/ulyaoth-tengine/SPECS/ulyaoth-tengine.spec"
-
+# Clean repository because AMI could have old data.
 if type dnf 2>/dev/null
 then
-  dnf builddep -y ulyaoth-tengine.spec
+  sudo dnf clean all
 elif type yum 2>/dev/null
 then
-  yum-builddep -y ulyaoth-tengine.spec
+  sudo yum clean all
 fi
 
+# Create build environment.
+rpmdev-setuptree
 
-su ulyaoth -c "spectool ulyaoth-tengine.spec -g -R"
-su ulyaoth -c "rpmbuild -ba ulyaoth-tengine.spec"
+# Download spec file.
+wget https://raw.githubusercontent.com/ulyaoth/repository/master/ulyaoth-tengine/SPECS/ulyaoth-tengine.spec -O /home/ulyaoth/rpmbuild/SPECS/ulyaoth-tengine.spec
 
-if [ "$ulyaothos" == "amazonlinux" ]
+# Install all requirements
+if type dnf 2>/dev/null
 then
-  cp /home/ulyaoth/rpmbuild/SRPMS/* /home/ec2-user/
-  cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /home/ec2-user/
-  cp /home/ulyaoth/rpmbuild/RPMS/i686/* /home/ec2-user/
-  cp /home/ulyaoth/rpmbuild/RPMS/i386/* /home/ec2-user/
-else
-  cp /home/ulyaoth/rpmbuild/SRPMS/* /root/
-  cp /home/ulyaoth/rpmbuild/RPMS/x86_64/* /root/
-  cp /home/ulyaoth/rpmbuild/RPMS/i686/* /root/
-  cp /home/ulyaoth/rpmbuild/RPMS/i386/* /root/
+  sudo dnf builddep -y /home/ulyaoth/rpmbuild/SPECS/ulyaoth-tengine.spec
+elif type yum 2>/dev/null
+then
+  sudo yum-builddep -y /home/ulyaoth/rpmbuild/SPECS/ulyaoth-tengine.spec
 fi
 
-rm -rf /root/build-ulyaoth-*
-rm -rf /home/ulyaoth/rpmbuild
-rm -rf /etc/nginx
+# Download additional files specified in spec file.
+spectool /home/ulyaoth/rpmbuild/SPECS/ulyaoth-tengine.spec -g -R
+
+# Build the rpm.
+rpmbuild -ba /home/ulyaoth/rpmbuild/SPECS/ulyaoth-tengine.spec
