6520aed5b054c06b4455338f9b2a8a5d70cce3c3
Sjir Bagmeijer <sbagmeijer@ulyaoth.net>
2017-11-30 19:23:46 +0100

    updated

---
+++ ulyaoth-jsvc/build-jsvc.sh
@@ -0,0 +1,13 @@
+# This script is supposed to run as the user "ulyaoth".
+
+# Create build environment.
+rpmdev-setuptree
+
+# Download spec file.
+wget https://raw.githubusercontent.com/ulyaoth/repository/master/ulyaoth-jsvc/SPECS/ulyaoth-jsvc.spec -O /home/ulyaoth/rpmbuild/SPECS/ulyaoth-jsvc.spec
+
+# Download additional files specified in spec file.
+spectool /home/ulyaoth/rpmbuild/SPECS/ulyaoth-jsvc.spec -g -R
+
+# Build the rpm.
+rpmbuild -ba /home/ulyaoth/rpmbuild/SPECS/ulyaoth-jsvc.spec
