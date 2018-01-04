cd9c6500d4f2cbef4816c24bd316e27467a8a7a4
Sjir Bagmeijer <sbagmeijer@ulyaoth.net>
2018-01-04 16:48:28 +0100

    amazonlinux 2 support

--- ulyaoth/build-ulyaoth.sh
+++ ulyaoth/build-ulyaoth.sh
@@ -1,7 +1,7 @@
 # This script is supposed to run as the user "ulyaoth".
 
 # Set required variables.
-ulyaothos=`cat /etc/ulyaoth | sed 's/[0-9]*//g'`
+ulyaothos=`cat /etc/ulyaoth`
 
 # Create build environment.
 rpmdev-setuptree
@@ -14,6 +14,12 @@ sed -i "s/sbagmeijer/$ulyaothos/g" /home/ulyaoth/rpmbuild/SPECS/ulyaoth.spec
 
 # Download additional files specified in spec file.
 spectool /home/ulyaoth/rpmbuild/SPECS/ulyaoth.spec -g -R
+
+# If Amazon Linux 2 change repo file.
+if [ "$ulyaothos" == "amazonlinux2" ]
+then
+  sed -i "s/amzn/amzn2/g" /home/ulyaoth/rpmbuild/SPECS/ulyaoth.spec
+fi
 
 # Build the rpm.
 rpmbuild -ba /home/ulyaoth/rpmbuild/SPECS/ulyaoth.spec
