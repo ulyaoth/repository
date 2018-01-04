19814095011156ce7ccc6a6be69662e66b93066c
Sjir Bagmeijer <sbagmeijer@ulyaoth.net>
2018-01-04 17:02:03 +0100

    amzn2

--- ulyaoth/build-ulyaoth.sh
+++ ulyaoth/build-ulyaoth.sh
@@ -15,11 +15,5 @@ sed -i "s/sbagmeijer/$ulyaothos/g" /home/ulyaoth/rpmbuild/SPECS/ulyaoth.spec
 # Download additional files specified in spec file.
 spectool /home/ulyaoth/rpmbuild/SPECS/ulyaoth.spec -g -R
 
-# If Amazon Linux 2 change repo file.
-if [ "$ulyaothos" == "amazonlinux2" ]
-then
-  sed -i "s/amzn/amzn2/g" /home/ulyaoth/rpmbuild/SOURCES/ulyaoth-amazonlinux.repo
-fi
-
 # Build the rpm.
 rpmbuild -ba /home/ulyaoth/rpmbuild/SPECS/ulyaoth.spec
